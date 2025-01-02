ce4d517e2f6703773b9ce2628b278e6b2f7f65f8
Felixnganga-max <felixngunga3620@gmail.com>
2025-01-02 15:07:03 +0300

    changed claudinary to 1.21.0

--- server-side/config/claudinary.js
+++ server-side/config/claudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config()
 
 cloudinary.config({
-  cloud_name: "dfuv6m4va",
-  api_key: "528637913152672",
-  api_secret: "kyLOb0R45nMaH-3c2AgxoDwRw1A",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 module.exports = {cloudinary};
